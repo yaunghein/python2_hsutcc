6161c2983a9e31c38c61062d919e54b7897ebf39
Yan Aung Hein <yan@apmhelp.com>
2025-11-12 01:03:15 +0700

    session one tasks

---
+++ day1.py
@@ -0,0 +1,16 @@
+# l = [1, 2, 3]
+# print([number * number for number in l])
+
+# print([number ** 2 if number % 3 == 1 else number // 2 for number in range(7)])
+
+# counter = [0]
+
+
+# def increment():
+#     # global counter
+#     counter += 1
+
+
+# increment()
+# import this
+# print(counter)
